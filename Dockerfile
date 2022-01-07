a6fc986ce38ee1135933c77eaa29cffd0ad5b19b
liquidsnk <liquidsnk@gmail.com>
2022-01-07 17:58:43 -0300

    first commit

---
+++ Dockerfile
@@ -0,0 +1,92 @@
+FROM mcr.microsoft.com/appsvc/php:7.4-apache_20210512.1
+
+# Install PHP extensions
+RUN apt-get --allow-releaseinfo-change update && apt-get install --no-install-recommends -y \
+    ca-certificates \
+    build-essential  \
+    software-properties-common \
+    cron \
+    git \
+    htop \
+    wget \
+    dos2unix \
+    curl \
+    libcurl4-gnutls-dev \
+    sudo \
+    libc-client-dev \
+    libkrb5-dev \
+    libmcrypt-dev \
+    libssl-dev \
+    libxml2-dev \
+    libzip-dev \
+    libjpeg-dev \
+    libmagickwand-dev \
+    libpng-dev \
+    libgif-dev \
+    libtiff-dev \
+    libz-dev \
+    libpq-dev \
+    imagemagick \
+    graphicsmagick \
+    libwebp-dev \
+    libjpeg62-turbo-dev \
+    libxpm-dev \
+    libaprutil1-dev \
+    libicu-dev \
+    libfreetype6-dev \
+    unzip \
+    nano \
+    zip \
+    mariadb-client \
+    && apt-get purge -y --auto-remove -o APT::AutoRemove::RecommendsImportant=false \
+    && rm -rf /var/lib/apt/lists/* \
+    && rm /etc/cron.daily/*
+
+RUN docker-php-ext-configure imap --with-kerberos --with-imap-ssl && \
+    docker-php-ext-install imap && \
+    docker-php-ext-enable imap
+
+RUN docker-php-ext-configure gd --with-freetype-dir=/usr/lib --with-png-dir=/usr/lib --with-jpeg-dir=/usr/lib \
+    && docker-php-ext-install  gd \
+    && docker-php-ext-configure opcache --enable-opcache \
+    && docker-php-ext-install intl mbstring mysqli curl pdo_mysql zip opcache bcmath gd \
+    && docker-php-ext-enable intl mbstring mysqli curl pdo_mysql zip opcache bcmath gd
+
+# Install composer
+RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/bin --filename=composer
+
+# By default disable cron jobs
+ENV MAUTIC_RUN_CRON_JOBS false
+
+# Setting an Default database user for Mysql
+ENV MAUTIC_DB_USER root
+
+# Setting an Default database name for Mysql
+ENV MAUTIC_DB_NAME mautic
+
+# Setting PHP properties
+ENV PHP_INI_DATE_TIMEZONE='UTC' \
+    PHP_MEMORY_LIMIT=512M \
+    PHP_MAX_UPLOAD=512M \
+    PHP_MAX_EXECUTION_TIME=300
+
+# Download Mautic from repository
+RUN git clone https://github.com/FishAngler/mautic.git /usr/src/mautic \
+    && chown -R www-data:www-data /usr/src/mautic
+
+# Install dependencies.
+RUN cd /usr/src/mautic && composer i -n ; cd -
+
+# Copy init scripts and custom .htaccess
+COPY docker-entrypoint.sh /entrypoint.sh
+COPY makeconfig.php /makeconfig.php
+COPY makedb.php /makedb.php
+COPY mautic.crontab /mautic.crontab
+RUN chmod 644 /mautic.crontab
+
+# Enable Apache Rewrite Module
+RUN a2enmod rewrite
+
+# Apply necessary permissions
+RUN ["chmod", "+x", "/entrypoint.sh"]
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ docker-entrypoint.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+
+set -e
+
+if [ ! -f /usr/local/etc/php/php.ini ]; then
+cat <<EOF > /usr/local/etc/php/php.ini
+date.timezone = "${PHP_INI_DATE_TIMEZONE}"
+always_populate_raw_post_data = -1
+memory_limit = ${PHP_MEMORY_LIMIT}
+file_uploads = On
+upload_max_filesize = ${PHP_MAX_UPLOAD}
+post_max_size = ${PHP_MAX_UPLOAD}
+max_execution_time = ${PHP_MAX_EXECUTION_TIME}
+EOF
+fi
+
+if [ -n "$MYSQL_PORT_3306_TCP" ]; then
+        if [ -z "$MAUTIC_DB_HOST" ]; then
+                export MAUTIC_DB_HOST='mysql'
+                if [ "$MAUTIC_DB_USER" = 'root' ] && [ -z "$MAUTIC_DB_PASSWORD" ]; then
+                        export MAUTIC_DB_PASSWORD="$MYSQL_ENV_MYSQL_ROOT_PASSWORD"
+                fi
+        else
+                echo "warning: both MAUTIC_DB_HOST and MYSQL_PORT_3306_TCP found"
+                echo "  Connecting to MAUTIC_DB_HOST ($MAUTIC_DB_HOST)"
+                echo "  instead of the linked mysql container"
+        fi
+fi
+
+if [ -z "$MAUTIC_DB_HOST" ]; then
+        echo >&2 "error: missing MAUTIC_DB_HOST and MYSQL_PORT_3306_TCP environment variables"
+        echo >&2 "  Did you forget to --link some_mysql_container:mysql or set an external db"
+        echo >&2 "  with -e MAUTIC_DB_HOST=hostname:port?"
+        exit 1
+fi
+
+if [ -z "$MAUTIC_DB_PASSWORD" ]; then
+        echo >&2 "error: missing required MAUTIC_DB_PASSWORD environment variable"
+        echo >&2 "  Did you forget to -e MAUTIC_DB_PASSWORD=... ?"
+        echo >&2
+        echo >&2 "  (Also of interest might be MAUTIC_DB_USER and MAUTIC_DB_NAME.)"
+        exit 1
+fi
+
+if ! [ -e index.php -a -e app/AppKernel.php ]; then
+       echo "Mautic not found in $pwd - copying now..."
+
+       tar cf - --one-file-system -C /usr/src/mautic . | tar xf -
+
+       echo "Complete! Mautic has been successfully copied to $pwd"
+fi
+
+# Ensure the MySQL Database is created
+php /makedb.php "$MAUTIC_DB_HOST" "$MAUTIC_DB_USER" "$MAUTIC_DB_PASSWORD" "$MAUTIC_DB_NAME"
+
+echo "========================================================================"
+echo ""
+echo "This server is now configured to run Mautic!"
+echo "The following information will be prefilled into the installer (keep password field empty):"
+echo "Host Name: $MAUTIC_DB_HOST"
+echo "Database Name: $MAUTIC_DB_NAME"
+echo "Database Username: $MAUTIC_DB_USER"
+
+# Write the database connection to the config so the installer prefills it
+if ! [ -e app/config/local.php ]; then
+        echo "Config not found in $pwd/app/config - creating now..."
+        php /makeconfig.php
+
+        # Make sure our web user owns the config file if it exists
+        chown www-data:www-data /home/site/wwwroot/app/config/local.php
+        mkdir -p /home/site/wwwroot/app/logs
+        chown www-data:www-data /home/site/wwwroot/app/logs
+        echo "Complete! Config has been created"
+fi
+
+if [[ "$MAUTIC_RUN_CRON_JOBS" == "true" ]]; then
+    if [ ! -e /var/log/cron.pipe ]; then
+        mkfifo /var/log/cron.pipe
+        chown www-data:www-data /var/log/cron.pipe
+    fi
+    (tail -f /var/log/cron.pipe | while read line; do echo "[CRON] $line"; done) &
+    CRONLOGPID=$!
+    cron -f &
+    CRONPID=$!
+
+    echo "Checking if mautic.crontab exists in /home"
+    if ! [ -e /home/mautic.crontab ]; then
+        echo "Moving mautic.crontab file to /home/mautic.crontab"
+        cp /mautic.crontab /home/mautic.crontab
+        chmod 644 /home/mautic.crontab
+    fi
+    #move the mautic cron job file to /etc/cron.d
+    echo "Moving mautic.crontab file to /etc/cron.d/mautic"
+    cp /home/mautic.crontab /etc/cron.d/mautic
+    chmod 644 /etc/cron.d/mautic
+    
+else
+    echo "Not running cron as requested."
+fi
+
+echo "========================================================================"
+
+"$@" &
+MAINPID=$!
+
+shut_down() {
+     echo "shutting down!!!!!!!!"
+     if [[ "$MAUTIC_RUN_CRON_JOBS" == "true" ]]; then
+         kill -TERM $CRONPID || echo 'Cron not killed. Already gone.'
+         kill -TERM $CRONLOGPID || echo 'Cron log not killed. Already gone.'
+     fi
+     kill -TERM $MAINPID || echo 'Main process not killed. Already gone.'
+}
+trap 'shut_down;' TERM INT
+
+echo "Executing Azure Entrypoint"
+source /bin/init_container.sh
